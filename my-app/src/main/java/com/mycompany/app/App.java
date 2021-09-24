785a345e0a5126ea8ed91245af0c70e076bc7728
Suyog <suyog.kulkarni1987@hotmail.com>
2021-09-25 02:51:53 +0530

    First Commit

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class App {
+
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("Hello, World! The current time is " + dtf.format(now));
+  }
+
+}
+
